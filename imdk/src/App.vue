ddddf1bdd91127466ade321d7aa0dcc4a4049f66
starkezz <592754817@qq.com>
2019-05-09 22:56:08 +0800

    首页初步

---
+++ imdk/src/App.vue
@@ -0,0 +1,45 @@
+<template>
+  <div id="app">
+    <div class="top">
+      <h5 class="top-title">
+        我来组成顶部
+      </h5>
+    </div>
+    <div>
+      welcome！
+    </div>
+    <router-view/>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "App"
+};
+</script>
+
+<style>
+#app {
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+}
+* {
+  margin: 0;
+  padding: 0;
+}
+.top{
+  height: 30px;
+  width: 100%;
+  background: #000033;
+  position: relative;
+  color: white
+}
+.top-title{
+  position: absolute;
+  top:50%;
+  left:50%;
+  transform: translateX(-50%);
+  transform: translateY(-50%);
+}
+</style>
