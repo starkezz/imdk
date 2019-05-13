327af7e4e03ab6ac921392b57421d690d485e99e
starkezz <592754817@qq.com>
2019-05-13 22:35:29 +0800

    重新提交

---
+++ imdk/src/components/HelloWorld.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="hello">
+    <h6>我来组成头部！</h6>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "HelloWorld",
+  data() {
+    return {};
+  }
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.hello {
+  height: 30px;
+  width: 100%;
+  background: pink;
+}
+</style>
