ca47b31ded8983ed02c9b3c74a1ca82dd3e24f25
starkezz <592754817@qq.com>
2019-05-29 22:21:27 +0800

    删除无用 文件

--- webpage/code/view/home/home.js
+++ webpage/code/view/home/home.js
@@ -1,3 +1,17 @@
 new Vue({
     el: "#idHome",
+    data() {
+        return {
+        }
+    },
+    methods: {
+        jump() {
+            let me = this;
+            
+            me.$su.view("#common/bug/edit")
+        }
+    },
+    created() {
+
+    }
 })
